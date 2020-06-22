96f1e5c9b9ebe8daf6d049f153ab404402dc9b7a
Zakaria Benharchache <zakariabenharchache@Zakarias-MacBook-Pro.local>
2020-06-21 19:26:43 -0700

    initial xcode project creation

---
+++ youtube-like-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-like-app
+//
+//  Created by Zakaria Benharchache on 6/21/20.
+//  Copyright © 2020 Zakaria Benharchache. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
